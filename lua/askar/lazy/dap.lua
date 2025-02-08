f9ba90ad1145078cdfe326645245dd294fdfcd47
Ask4r <dv4askar@gmail.com>
2025-02-08 16:02:27 +0300

    DAP config

--- lua/askar/lazy/dap.lua
+++ lua/askar/lazy/dap.lua
@@ -1,136 +1,172 @@
+local curr_dirname = function()
+    return string.match(vim.fn.getcwd(), ".*/([%w-_]+)")
+end
+
+local zig_exe_path = function()
+    -- return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
+    local cwd = vim.fn.getcwd()
+    local exe_path = cwd .. "/zig-out/bin/" .. curr_dirname()
+    return exe_path
+end
+
 return {
-    {
-        "mfussenegger/nvim-dap",
-        dependencies = {
-            "leoluz/nvim-dap-go",
-            "rcarriga/nvim-dap-ui",
-            "theHamsta/nvim-dap-virtual-text",
-            "nvim-neotest/nvim-nio",
-            "williamboman/mason.nvim",
-        },
-        config = function()
-            local dap = require "dap"
-            local ui = require "dapui"
-
-            require("dapui").setup()
-            require("dap-go").setup()
-
-            require("nvim-dap-virtual-text").setup {
-                -- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
-                display_callback = function(variable)
-                    local name = string.lower(variable.name)
-                    local value = string.lower(variable.value)
-                    if name:match "secret" or name:match "api" or value:match "secret" or value:match "api" then
-                        return "*****"
-                    end
-
-                    if #variable.value > 15 then
-                        return " " .. string.sub(variable.value, 1, 15) .. "... "
-                    end
-
-                    return " " .. variable.value
-                end,
-            }
-
-            -- Handled by nvim-dap-go
-            -- dap.adapters.go = {
-            --   type = "server",
-            --   port = "${port}",
-            --   executable = {
-            --     command = "dlv",
-            --     args = { "dap", "-l", "127.0.0.1:${port}" },
-            --   },
-            -- }
-
-            local elixir_ls_debugger = vim.fn.exepath "elixir-ls-debugger"
-            if elixir_ls_debugger ~= "" then
-                dap.adapters.mix_task = {
-                    type = "executable",
-                    command = elixir_ls_debugger,
-                }
-
-                dap.configurations.elixir = {
-                    {
-                        type = "mix_task",
-                        name = "phoenix server",
-                        task = "phx.server",
-                        request = "launch",
-                        projectDir = "${workspaceFolder}",
-                        exitAfterTaskReturns = false,
-                        debugAutoInterpretAllModules = false,
-                    },
-                }
-            end
-
-            dap.adapters.gdb = {
-                type = "executable",
-                command = "gdb",
-                args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
-            }
-
-            dap.configurations.c = {
-                {
-                    name = "Launch",
-                    type = "gdb",
-                    request = "launch",
-                    program = function()
-                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-                    end,
-                    cwd = "${workspaceFolder}",
-                    stopAtBeginningOfMainSubprogram = false,
-                },
-                {
-                    name = "Select and attach to process",
-                    type = "gdb",
-                    request = "attach",
-                    program = function()
-                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-                    end,
-                    pid = function()
-                        local name = vim.fn.input('Executable name (filter): ')
-                        return require("dap.utils").pick_process({ filter = name })
-                    end,
-                    cwd = '${workspaceFolder}'
-                },
-                {
-                    name = 'Attach to gdbserver :1234',
-                    type = 'gdb',
-                    request = 'attach',
-                    target = 'localhost:1234',
-                    program = function()
-                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-                    end,
-                    cwd = '${workspaceFolder}'
-                },
-            }
-
-            vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
-            vim.keymap.set("n", "<leader>gb", dap.run_to_cursor)
-
-            -- Eval var under cursor
-            vim.keymap.set("n", "<leader>?", function()
-                require("dapui").eval(nil, { enter = true })
-            end)
-
-            vim.keymap.set("n", "<F1>", dap.continue)
-            vim.keymap.set("n", "<F2>", dap.step_into)
-            vim.keymap.set("n", "<F3>", dap.step_over)
-            vim.keymap.set("n", "<F4>", dap.step_out)
-            vim.keymap.set("n", "<F5>", dap.step_back)
-            vim.keymap.set("n", "<F12>", dap.restart)
-
-            dap.listeners.before.attach.dapui_config = function()
-                ui.open()
-            end
-            dap.listeners.before.launch.dapui_config = function()
-                ui.open()
-            end
-            dap.listeners.before.event_terminated.dapui_config = function()
-                ui.close()
-            end
-            dap.listeners.before.event_exited.dapui_config = function()
-                ui.close()
-            end
-        end,
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "leoluz/nvim-dap-go",
+        "nicholasmata/nvim-dap-cs",
+        "rcarriga/nvim-dap-ui",
+        "theHamsta/nvim-dap-virtual-text",
+        "nvim-neotest/nvim-nio",
+        "williamboman/mason.nvim",
     },
+    config = function()
+        local dap = require("dap")
+        local dapui = require("dapui")
+
+        require("dapui").setup()
+        require("dap-go").setup()
+        require("dap-cs").setup({
+            netcoredbg = {
+                path = vim.fn.expand("$HOME/.local/share/nvim/mason/bin/netcoredbg"),
+            },
+        })
+
+        require("nvim-dap-virtual-text").setup({
+            -- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
+            display_callback = function(variable)
+                local name = string.lower(variable.name)
+                local value = string.lower(variable.value)
+                if name:match "secret" or name:match "api" or value:match "secret" or value:match "api" then
+                    return "*****"
+                end
+
+                if #variable.value > 15 then
+                    return " " .. string.sub(variable.value, 1, 15) .. "... "
+                end
+
+                return " " .. variable.value
+            end,
+        })
+
+        -- Handled by nvim-dap-go
+        -- dap.adapters.go = {
+        --     type = "server",
+        --     port = "${port}",
+        --     executable = {
+        --         command = "dlv",
+        --         args = { "dap", "-l", "127.0.0.1:${port}" },
+        --     },
+        -- }
+
+        local elixir_ls_debugger = vim.fn.exepath("elixir-ls-debugger")
+        if elixir_ls_debugger ~= "" then
+            dap.adapters.mix_task = {
+                type = "executable",
+                command = elixir_ls_debugger,
+            }
+
+            dap.configurations.elixir = {
+                {
+                    type = "mix_task",
+                    name = "phoenix server",
+                    task = "phx.server",
+                    request = "launch",
+                    projectDir = "${workspaceFolder}",
+                    exitAfterTaskReturns = false,
+                    debugAutoInterpretAllModules = false,
+                },
+            }
+        end
+
+        dap.adapters.gdb = {
+            type = "executable",
+            command = "gdb",
+            args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
+        }
+
+        dap.adapters.codelldb = {
+            type = "server",
+            port = "${port}",
+            executable = {
+                command = vim.fn.expand("$HOME/.local/share/nvim/mason/bin/codelldb"), -- I installed codelldb through mason.nvim
+                args = { "--port", "${port}" },
+            },
+        }
+
+        dap.configurations.c = {
+            {
+                name = "Launch",
+                type = "gdb",
+                request = "launch",
+                program = function()
+                    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                end,
+                cwd = "${workspaceFolder}",
+                stopAtBeginningOfMainSubprogram = false,
+            },
+            {
+                name = "Select and attach to process",
+                type = "gdb",
+                request = "attach",
+                program = function()
+                    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                end,
+                pid = function()
+                    local name = vim.fn.input('Executable name (filter): ')
+                    return require("dap.utils").pick_process({ filter = name })
+                end,
+                cwd = '${workspaceFolder}'
+            },
+            {
+                name = 'Attach to gdbserver :1234',
+                type = 'gdb',
+                request = 'attach',
+                target = 'localhost:1234',
+                program = function()
+                    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                end,
+                cwd = '${workspaceFolder}'
+            },
+        }
+
+        dap.configurations.zig = {
+            {
+                name = "Launch",
+                type = "codelldb",
+                request = "launch",
+                program = zig_exe_path,
+                cwd = "${workspaceFolder}",
+                stopOnEntry = false,
+            },
+        }
+
+
+        vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint)
+        vim.keymap.set("n", "<leader>gb", dap.run_to_cursor)
+
+        -- Eval var under cursor
+        vim.keymap.set("n", "<leader>?", function()
+            require("dapui").eval(nil, { enter = true })
+        end)
+
+        vim.keymap.set("n", "<leader>dc", dap.continue)
+        vim.keymap.set("n", "<C-[>", dap.step_into)
+        vim.keymap.set("n", "<C-]>", dap.step_over)
+        vim.keymap.set("n", "<leader>do", dap.step_out)
+        vim.keymap.set("n", "<leader>dsb", dap.step_back)
+        vim.keymap.set("n", "<leader>dr", dap.restart)
+
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
+    end,
 }
